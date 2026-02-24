1338a382fb5bd9153f3b75de110bc5a7ff45085c
jesus1035 <jesus480b@gmail.com>
2026-02-24 16:14:52 -0600

    Implementacion de Repository e Interfaz service realizadas

---
+++ huespedes-mvs/src/main/java/com/example/huespedes/service/HuespedService.java
@@ -0,0 +1,10 @@
+package com.example.huespedes.service;
+
+import com.example.common.dto.huespede.HuespedRequest;
+import com.example.common.dto.huespede.HuespedResponse;
+import com.example.common.services.CrudServices;
+
+public interface HuespedService extends CrudServices<HuespedRequest, HuespedResponse>{
+	
+
+}
