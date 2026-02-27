e205425107e8c672b0e5fd08a4931c06e7acbf17
ChristianLeonardo <leonardonava2400@gmail.com>
2026-02-26 18:44:43 -0600

    Ya ahora si esta redy

--- reservas-mvs/src/main/java/com/example/reservas/controllers/ReservaController.java
+++ reservas-mvs/src/main/java/com/example/reservas/controllers/ReservaController.java
@@ -1,6 +1,9 @@
 package com.example.reservas.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.common.controller.CommonController;
 import com.example.common.dto.ReservaRequest;
 import com.example.common.dto.ReservaResponse;
+import com.example.common.enums.EstadoReserva;
+import com.example.reservas.repositories.ReservaRepository;
 import com.example.reservas.services.ReservaService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +21,11 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class ReservaController extends CommonController<ReservaRequest, ReservaResponse, ReservaService> {
 
-    public ReservaController(ReservaService service) {
+    private final ReservaRepository reservaRepository;
+
+    public ReservaController(ReservaService service, ReservaRepository reservaRepository) {
         super(service);
+        this.reservaRepository = reservaRepository;
     }
 
     @PatchMapping("/{idReserva}/estado/{idEstado}")
@@ -26,5 +34,13 @@ public class ReservaController extends CommonController<ReservaRequest, ReservaR
             @PathVariable Integer idEstado) {
         log.info("Cambiando estado de reserva {} a {}", idReserva, idEstado);
         return ResponseEntity.ok(service.cambiarEstado(idReserva, idEstado));
+    }
+    
+    
+    @GetMapping("/huesped/{idHuesped}/tiene-activas")
+    public ResponseEntity<Boolean> huespedTieneReservasActivas(@PathVariable Long idHuesped) {
+        return ResponseEntity.ok(reservaRepository.existsByIdHuespedAndEstadoReservaIn(
+                idHuesped,
+                List.of(EstadoReserva.CONFIRMADA, EstadoReserva.EN_CURSO)));
     }
 }
